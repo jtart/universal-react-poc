40c93f809b2588a46b700d83bc0cf99e00e82d21
Jordan Tart <jordan.tart@bbc.co.uk>
2018-05-11 15:07:08 +0100

    Add Link

--- src/app/components/Article.jsx
+++ src/app/components/Article.jsx
@@ -2,11 +2,13 @@
 
 import * as React from 'react';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
 
 const Article = (): React.Node => (
   <div>
     <Helmet title="Article!" />
     <h1>Article content :)</h1>
+    <Link to="/">Another article</Link>
   </div>
 );
 
