f146b6517348b1b2c78680d45a2f87a988b07cc6
Jordan Tart <jordan.tart@bbc.co.uk>
2018-05-11 16:30:55 +0100

    Optimise chunking

--- razzle.config.js
+++ razzle.config.js
@@ -1,9 +1,9 @@
 'use strict';
 const { dependencies } = require('./package.json');
 
-const vendorPackages = Object.keys(dependencies)
-  .filter(dep => dep !== 'express')
-  .map(dep => require.resolve(dep));
+const vendorPackages = Object.keys(dependencies).filter(
+  dep => dep !== 'express',
+);
 
 module.exports = {
   modify(defaultConfig, { target, dev }, webpack) {
@@ -14,16 +14,20 @@ module.exports = {
         ? 'static/js/[name].js'
         : 'static/js/[name].[hash:8].js';
 
-      config.entry.vendor = [
-        require.resolve('razzle/polyfills'),
-        ...vendorPackages,
-      ];
+      config.entry.vendor = ['razzle/polyfills', ...vendorPackages];
 
       config.optimization = {
         splitChunks: {
-          chunks: 'all',
-          name: false,
+          cacheGroups: {
+            vendor: {
+              chunks: 'initial',
+              name: 'vendor',
+              test: /[\\/]node_modules[\\/]/,
+              reuseExistingChunk: true,
+            },
+          },
         },
+        runtimeChunk: true,
       };
     }
 
--- src/client.js
+++ src/client.js
@@ -1,1 +1,16 @@
-import('./client');
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import routes from './app/routes';
+
+const Router = <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>;
+const rootElement = document.getElementById('root');
+
+if (rootElement !== null) {
+  hydrate(Router, rootElement);
+}
+
+if (module.hot) {
+  module.hot.accept();
+}
